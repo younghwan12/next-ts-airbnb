b0c55fb2cd8a68cbf2288faf9eb63ce146c3ebd7
younghwan12 <dowk3432@gmail.com>
2023-04-06 13:42:46 +0900

    mongodb 추가

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -8,6 +8,11 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 import useRegisterModal from "@/app/hooks/useRegisterModal";
 import Modal from "./Modal";
+import Heading from "../Heading";
+import Input from "../inputs/Input";
+
+import { toast } from "react-hot-toast";
+import Button from "../Button";
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
@@ -34,12 +39,75 @@ const RegisterModal = () => {
                 registerModal.onClose();
             })
             .catch((error) => {
-                console.log(error);
+                toast.error("Something went Wrong");
             })
             .finally(() => {
                 setIsLoading(false);
             });
     };
+
+    const bodyContent = (
+        <div className="flex flex-col gap-4">
+            <Heading
+                title="에어비앤비에 오신 것을 환영합니다."
+                subtitle="Create an account!"
+            />
+            <Input
+                id="email"
+                label="Email"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+            <Input
+                id="name"
+                label="Name"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+            <Input
+                id="password"
+                type="password"
+                label="Password"
+                disabled={isLoading}
+                register={register}
+                errors={errors}
+                required
+            />
+        </div>
+    );
+
+    const footerContent = (
+        <div className="flex flex-col gap-4 mt-3">
+            <hr />
+            <Button
+                outline
+                label="구글로 로그인하기"
+                icon={FcGoogle}
+                onClick={() => {}}
+            />
+            <Button
+                outline
+                label="깃허브로 로그인하기"
+                icon={AiFillGithub}
+                onClick={() => {}}
+            />
+            <div className="text-neutral-400 text-center mt-4 font-light">
+                <div className="justify-center flex flex-row items-center gap-2">
+                    <div>이미 계정이 있으신가요?</div>
+                    <div
+                        onClick={registerModal.onClose}
+                        className="text-neutral-800 cursor-pointer hover:underline"
+                    >
+                        로그인
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 
     return (
         <Modal
@@ -49,6 +117,8 @@ const RegisterModal = () => {
             actionLabel="Continue"
             onClose={registerModal.onClose}
             onSubmit={handleSubmit(onSubmit)}
+            body={bodyContent}
+            footer={footerContent}
         />
     );
 };
